e2ccd57c9c276284b241efe0ef56a7fe313aa502
Vishal <123442668+Tringapps-vishal@users.noreply.github.com>
2023-01-31 11:03:39 +0530

    Add files via upload

---
+++ 2D-space.java
@@ -0,0 +1,46 @@
+// "2. Design a class for a basic point in 2D space. The class should have the following features:
+//     - Instance variables for the x and y coordinates of the point.
+//     - A constructor that allows the user to create a point with given x and y coordinates.
+//     - An equals method that returns true if the given object is a point with the same x and y coordinates, and false otherwise.
+//     - A clone method that creates and returns a new point with the same x and y coordinates as the original point.
+// "
+import java.util.*;
+class point implements Cloneable {
+    int x2, y2;
+    boolean result;
+    point(int x1, int y1) {
+        x2 = x1;
+        y2 = y1;
+    }
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+    boolean equals(int x1, int y1) {
+        if ((x2 == x1) && (y2 == y1)){
+        result=true;}
+        else{
+        result=false;}
+        return result;
+    }
+}
+
+class getdata {
+    public static void main(String args[]) throws CloneNotSupportedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf(" ------1st point-------  \n");
+        System.out.printf("\n Enter X value : ");
+        int x1 = sc.nextInt();
+        System.out.printf(" Enter Y value : ");
+        int y1 = sc.nextInt();
+        point p = new point(x1, y1);
+        point p1 = (point) p.clone();
+        System.out.printf("\n --------2nd poind------ \n");
+        System.out.printf("\n Enter X value: ");
+        int x2 = sc.nextInt();
+        System.out.printf(" Enter Y value : ");
+        int y2 = sc.nextInt();
+        boolean ans = p1.equals(x2, y2);
+        System.out.printf("" + ans); 
+    }
+}
